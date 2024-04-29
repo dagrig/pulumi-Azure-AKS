c57f3a35439dfbfd324e6c0a81852f658ee75311
David Grigoryan <56737762+dagrig@users.noreply.github.com>
2024-04-28 20:09:06 -0400

    initial

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# pulumi-Azure-AKS
+# Pulumi AKS Project
+
+This repository contains a Pulumi project for deploying an Azure Kubernetes Service (AKS) cluster using Python.
+
+## Prerequisites
+
+- Pulumi
+- Azure CLI
+- Python 3.x
+
+## Setup
+
+- Clone this repository.
+- Navigate into the repository directory.
+- Install dependencies:
+  ```bash
+  pip install -r requirements.txt
---
+++ __main__.py
@@ -0,0 +1,60 @@
+import pulumi
+from pulumi_azure_native import resources, containerservice
+from pulumi_azure_native.containerservice import ManagedCluster, ManagedClusterServicePrincipalProfile
+from pulumi_azure_native.resources import ResourceGroup
+from pulumi_kubernetes import Provider
+from pulumi_kubernetes.helm.v3 import Chart, ChartOpts
+
+# Create a new resource group
+resource_group = ResourceGroup('rg')
+
+# Create an AKS cluster
+aks_cluster = ManagedCluster(
+    'aksCluster',
+    resource_group_name=resource_group.name,
+    agent_pool_profiles=[{
+        'count': 2,
+        'max_pods': 110,
+        'mode': 'System',
+        'name': 'agentpool',
+        'os_type': 'Linux',
+        'vm_size': 'Standard_DS2_v2',
+    }],
+    dns_prefix='aksCluster',
+    linux_profile={
+        'admin_username': 'adminuser',
+        'ssh': {
+            'publicKeys': [{
+                'keyData': 'ssh-rsa AAAAB3Nza...'
+            }]
+        }
+    },
+    service_principal_profile=ManagedClusterServicePrincipalProfile(
+        client_id='your-client-id',
+        secret='your-client-secret',
+    )
+)
+
+# Create a Kubernetes provider instance using the kubeconfig from the AKS cluster
+k8s_provider = Provider(
+    'k8sProvider',
+    kubeconfig=aks_cluster.kube_config_raw,
+)
+
+# Deploy an NGINX Helm chart using the Kubernetes provider
+nginx_chart = Chart(
+    'nginx',
+    ChartOpts(
+        chart='nginx',
+        version='9.3.7',  # make sure to check for the latest version
+        fetch_opts={
+            'repo': 'https://charts.bitnami.com/bitnami',
+        },
+    ),
+    opts=pulumi.ResourceOptions(provider=k8s_provider)
+)
+
+# Export the kubeconfig and the public IP of the NGINX service
+pulumi.export('kubeconfig', aks_cluster.kube_config_raw)
+nginx_service = nginx_chart.get_resource('v1/Service', 'nginx-nginx')
+pulumi.export('nginx_ip', nginx_service.status.apply(lambda status: status.load_balancer.ingress[0].ip))
