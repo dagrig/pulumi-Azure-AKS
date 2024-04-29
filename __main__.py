f869495f06025373f49188ad29831e58bdd543d6
David Grigoryan <56737762+dagrig@users.noreply.github.com>
2024-04-28 20:43:56 -0400

    Added Karpenter for faster pod scheduling

--- __main__.py
+++ __main__.py
@@ -13,7 +13,7 @@ aks_cluster = ManagedCluster(
     'aksCluster',
     resource_group_name=resource_group.name,
     agent_pool_profiles=[{
-        'count': 2,
+        'count': 3,
         'max_pods': 110,
         'mode': 'System',
         'name': 'agentpool',
@@ -46,7 +46,7 @@ nginx_chart = Chart(
     'nginx',
     ChartOpts(
         chart='nginx',
-        version='9.3.7',  # make sure to check for the latest version
+        version='9.3.7',  # Make sure to check for the latest version
         fetch_opts={
             'repo': 'https://charts.bitnami.com/bitnami',
         },
@@ -54,7 +54,28 @@ nginx_chart = Chart(
     opts=pulumi.ResourceOptions(provider=k8s_provider)
 )
 
+# Install Karpenter
+karpenter_chart = Chart(
+    "karpenter",
+    ChartOpts(
+        chart="karpenter",
+        version="0.11.1",  # Check for the latest version from Karpenter's Helm repository
+        namespace="karpenter",
+        fetch_opts={
+            "repo": "https://charts.karpenter.sh",
+        },
+        values={
+            "clusterName": aks_cluster.name,
+            "clusterEndpoint": aks_cluster.private_fqdn,
+        }
+    ),
+    opts=pulumi.ResourceOptions(provider=k8s_provider, depends_on=[nginx_chart])
+)
+
 # Export the kubeconfig and the public IP of the NGINX service
 pulumi.export('kubeconfig', aks_cluster.kube_config_raw)
 nginx_service = nginx_chart.get_resource('v1/Service', 'nginx-nginx')
 pulumi.export('nginx_ip', nginx_service.status.apply(lambda status: status.load_balancer.ingress[0].ip))
+
+# Export Karpenter chart status
+pulumi.export('karpenter_chart_status', karpenter_chart.status)
